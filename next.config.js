95cf328d28063ba0c48df723908258f9caf60b04
weekendfish <yusen356@gmail.com>
2025-02-08 19:44:52 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
         unoptimized: true,
     },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    headers: async () => {
-        return [
-            {
-                source: '/sw.js',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=0, must-revalidate',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig; 
