35b0262b259f9fbd405000673e4683b323baf97f
weekendfish <yusen356@gmail.com>
2025-02-08 20:06:52 +0000

    dddd

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    trailingSlash: true,
 };
 
 module.exports = nextConfig; 
